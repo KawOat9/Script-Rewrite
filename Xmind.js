9e43080abe807bb23a366a0cab1f5c44633d0f98
@KawOat9 <boonseub85@gmail.com>
2025-12-19 22:26:45 +0700

    Update Xmind.js

--- Xmind.js
+++ Xmind.js
@@ -21,6 +21,17 @@ hostname = *xmind.*
 
 // 主脚本函数...
 
+var body = $response.body;
+var objk = JSON.parse(body);
+
+objk = {"raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=", "license": {"status": "sub", "expireTime":3070928235000}, "_code": 200}
+	
+
+body = JSON.stringify(objk);
+
+$done({body});
+
+/*
 var chxm1023 = JSON.parse($response.body);
 const vip = /https:\/\/www\.xmind\.cn\/_res\/devices/;
 const token = /https:\/\/xmind\.cn\/_res\/token\/.+/;
