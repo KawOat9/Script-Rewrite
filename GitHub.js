e8dfb1f1437591c9e8ff77d09a4b90aa23fe5992
@KawOat9 <boonseub85@gmail.com>
2026-01-03 08:59:10 +0700

    Update GitHub.js and GitHub.sgmodule

---
+++ GitHub.js
@@ -0,0 +1,40 @@
+/*
+#!name= GitHub ✨
+#!desc=脚本仓库
+#!category=🔐APP
+#!author=🅚Ⓐ🅦Ⓞ🅐Ⓣ
+#!icon=https://raw.githubusercontent.com/KawOat9/icons/main/Nordic/Github.jpg
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+[rewrite_local]
+https://api.github.com/graphql url script-response-body https://raw.githubusercontent.com/KawOat9/Scripts/main/GitHub.js
+
+[MITM]
+hostname = api.github.com
+
+*/
+
+// -------- 通知（带冷却）逻辑开始 --------
+const cooldownMs = 10 * 60 * 1000;
+const notifyKey = "GitHub_notify_key";
+const now = Date.now();
+let lastNotifyTime = $persistentStore.read(notifyKey) ? parseInt($persistentStore.read(notifyKey)) : 0;
+if (now - lastNotifyTime > cooldownMs) {
+    $notification.post("✨GitHub✨", "🅚Ⓐ🅦Ⓞ🅐Ⓣ", "หมดอายุ: ⓿❽-⓿❽-❷⓿❽❽");
+    $persistentStore.write(now.toString(), notifyKey);
+}
+// -------- 通知（带冷却）逻辑结束 --------
+
+// 主脚本函数...
+let obj = JSON.parse($response.body);
+if (obj.data && obj.data.viewer) {
+  obj.data.viewer.isProPlan = true;
+  obj.data.viewer.isEmployee = true;
+  obj.data.viewer.hasAppleIapSubscription = true;
+
+  $done({
+    body: JSON.stringify(obj)
+  });
+} else {
+  $done({});
+}
+// 主脚本函数...
